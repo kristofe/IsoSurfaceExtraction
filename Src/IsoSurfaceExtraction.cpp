1c35068932599e31aab0a8b0ba37e657715cb930
Kristofer Schlachter <kristofe72@gmail.com>
2020-12-21 13:33:29 -0500

    Cut down version of code for python wrapping is started and works

--- Src/IsoSurfaceExtraction.cpp
+++ Src/IsoSurfaceExtraction.cpp
@@ -32,57 +32,20 @@ DAMAGE.
 #include <unordered_map>
 #include <algorithm>
 #include <sys/timeb.h>
-#ifndef WIN32
-#include <sys/time.h>
-#endif // WIN32
 
-#include <Src/CmdLineParser.h>
+#include <cstring>
 #include <Src/Geometry.h>
 #include <Src/Ply.h>
 #include <Src/MarchingCubes.h>
 #include <Src/Array.h>
 
-
-const float DEFAULT_DIMENSIONS[] = { 1.f , 1.f , 1.f };
-cmdLineParameter< char* > In( "in" ) , Out( "out" );
-cmdLineParameterArray< int , 3 > Resolution( "res" );
-cmdLineParameter< int > SmoothIterations( "sIters" , 0 );
-cmdLineParameter< float > IsoValue( "iso" , 0.f );
-cmdLineParameterArray< float , 3 > Dimensions( "dim" , DEFAULT_DIMENSIONS );
-cmdLineReadable Float( "float" ) , FullCaseTable( "full" ) , FlipOrientation( "flip" ) , QuadraticFit( "quadratic" ) , Polygons( "polygons" ) , NonManifold( "nonManifold" ) , FlipBytes( "flipBytes" );
-
-cmdLineReadable* params[] = { &In , &Out , &Resolution , &IsoValue , &FullCaseTable , &FlipOrientation , &QuadraticFit , &Polygons , &SmoothIterations , &Float , &Dimensions , &NonManifold , &FlipBytes , NULL };
-
-void ShowUsage( const char* ex )
+struct TripleInt
 {
-	printf( "Usage %s:\n" , ex );
-	printf( "\t --%s <input voxel grid>\n" , In.name );
-	printf( "\t[--%s <input resolution>]\n" , Resolution.name );
-	printf( "\t[--%s <output iso-surface>]\n" , Out.name );
-	printf( "\t[--%s <iso-value>=%f]\n" , IsoValue.name , IsoValue.value );
-	printf( "\t[--%s <smoothing iterations>=%d]\n" , SmoothIterations.name , SmoothIterations.value );
-	printf( "\t[--%s <dimensions of a voxel>=%f %f %f]\n" , Dimensions.name , Dimensions.values[0] , Dimensions.values[1] , Dimensions.values[2] );
-	printf( "\t[--%s]\n" , FullCaseTable.name );
-	printf( "\t[--%s]\n" , FlipOrientation.name );
-	printf( "\t[--%s]\n" , QuadraticFit.name );
-	printf( "\t[--%s]\n" , Polygons.name );
-	printf( "\t[--%s]\n" , NonManifold.name );
-	printf( "\t[--%s]\n" , Float.name );
-	printf( "\t[--%s]\n" , FlipBytes.name );
-}
-
-inline double Time( void )
-{
-#ifdef WIN32
-	struct _timeb t;
-	_ftime( &t );
-	return double( t.time ) + double( t.millitm ) / 1000.0;
-#else // WIN32
-	struct timeval t;
-	gettimeofday( &t , NULL );
-	return t.tv_sec + double( t.tv_usec ) / 1000000;
-#endif // WIN32
-}
+public:
+	int values[3];
+};
+struct TripleInt Resolution;
+struct TripleInt Dimensions;
 
 
 float    LinearInterpolant( float x1 , float x2 , float isoValue ){ return ( isoValue-x1 ) / ( x2-x1 ); }
@@ -321,167 +284,69 @@ void ExtractIsoSurface( int resX , int resY , int resZ , ConstPointer( float ) v
 }
 int main( int argc , char* argv[] )
 {
-	cmdLineParse( argc-1 , argv+1 , params );
-	if( !In.set ){ ShowUsage( argv[0] ) ; return EXIT_FAILURE; }
+	float IsoValue = 0.0f;
+	bool FullCaseTable = false;
+	bool QuadraticFit = true;
+	bool FlipOrientation = false;
 
-	FILE* fp = fopen( In.value , "rb" );
+	const char* in_path = "raw_voxel_data.dat";
+	const char* out_path = "test.ply";
+	FILE* fp = fopen( in_path, "rb" );
 	if( !fp )
 	{
-		fprintf( stderr , "[ERROR] Failed to open file for reading: %s\n" , In.value );
+		fprintf( stderr , "[ERROR] Failed to open file for reading: %s\n" , in_path);
 		return EXIT_FAILURE;
 	}
-	if( !Resolution.set )
+	int res;
+	if( fread( &res , sizeof(int) , 1 ,fp )!=1 )
 	{
-		int res;
-		if( fread( &res , sizeof(int) , 1 ,fp )!=1 )
-		{
-			fprintf( stderr , "[ERROR] Failed to read voxel grid resolution from file.\n" );
-			return EXIT_FAILURE;
-		}
-		Resolution.values[0] = Resolution.values[1] = Resolution.values[2] = res;
+		fprintf( stderr , "[ERROR] Failed to read voxel grid resolution from file.\n" );
+		return EXIT_FAILURE;
+	}else{
+		printf("Found voxel file with dim %d %d %d", res, res, res);
 	}
+	
 
-	Pointer( float ) voxelValues = NewPointer< float >( Resolution.values[0] * Resolution.values[1] * Resolution.values[2] );
+	Pointer( float ) voxelValues = NewPointer< float >(res*res*res);
 	if( !voxelValues )
 	{
-		fprintf( stderr , "[ERROR] Failed to allocte voxel grid: %d x %d x %d\n" , Resolution.values[0] , Resolution.values[1] , Resolution.values[2] );
+		fprintf( stderr , "[ERROR] Failed to allocte voxel grid: %d x %d x %d\n" , res , res, res);
 		fclose( fp );
 		return EXIT_FAILURE;
 	}
 
-	if( Float.set )
+	if( fread( voxelValues , sizeof( float ) , res*res*res, fp )!=res*res*res)
 	{
-		if( fread( voxelValues , sizeof( float ) , Resolution.values[0] * Resolution.values[1] * Resolution.values[2] , fp )!=Resolution.values[0]*Resolution.values[1]*Resolution.values[2] )
-		{
-			fprintf( stderr , "[ERROR] Failed to read voxel grid from file.\n" );
-			return EXIT_FAILURE;
-		}
-		if( FlipBytes.set )
-		{
-			Pointer( unsigned char ) _voxelValues = ( Pointer( unsigned char ) )voxelValues;
-			for( int i=0 ; i<Resolution.values[0] * Resolution.values[1] * Resolution.values[2] ; i++ ) for( int j=0 ; j<sizeof(float)/2 ; j++ )
-			{
-				unsigned char foo = _voxelValues[ i*sizeof(float) + j ];
-				_voxelValues[ i*sizeof(float) + j ] = _voxelValues[ i*sizeof(float) + sizeof(float) - 1 - j ];
-				_voxelValues[ i*sizeof(float) + sizeof(float) - 1 - j ] = foo;
-			}
-		}
-	}
-	else
-	{
-		Pointer( unsigned char ) _voxelValues = NewPointer< unsigned char >( Resolution.values[0] * Resolution.values[1] * Resolution.values[2] );
-		if( fread( _voxelValues , sizeof( unsigned char ) , Resolution.values[0] * Resolution.values[1] * Resolution.values[2] , fp )!=Resolution.values[0]*Resolution.values[1]*Resolution.values[2] )
-		{
-			fprintf( stderr , "[ERROR] Failed to read voxel grid from file.\n" );
-			return EXIT_FAILURE;
-		}
-#pragma omp parallel for
-		for( int i=0 ; i<Resolution.values[0]*Resolution.values[1]*Resolution.values[2] ; i++ ) voxelValues[i] = (float)_voxelValues[i];
-		DeletePointer( _voxelValues );
+		fprintf( stderr , "[ERROR] Failed to read voxel grid from file.\n" );
+		return EXIT_FAILURE;
 	}
 	fclose( fp );
 
 
-#define INDEX( x , y , z ) ( (x) + (y)*Resolution.values[0] + (z)*Resolution.values[0]*Resolution.values[1] )
+#define INDEX( x , y , z ) ( (x) + (y)*res+ (z)*res*res )
 
 	float min , max;
 	min = max = voxelValues[0];
-	for( int x=0 ; x<Resolution.values[0] ; x++ ) for( int y=0 ; y<Resolution.values[1] ; y++ ) for( int z=0 ; z<Resolution.values[2] ; z++ )
+	for( int x=0 ; x<res ; x++ ) for( int y=0 ; y<res ; y++ ) for( int z=0 ; z<res; z++ )
 		min = std::min< float >( min , voxelValues[ INDEX(x,y,z) ] ) , max = std::max< float >( max , voxelValues[ INDEX(x,y,z) ] );
 	printf( "Value Range: [%f,%f]\n" , min , max );
-
-	if( SmoothIterations.value>0 )
-	{
-		float stencil[] = { 0.5f , 1.f , 0.5f };
-		Pointer( float ) _voxelValues = NewPointer< float >( Resolution.values[0] * Resolution.values[1] * Resolution.values[2] );
-		for( int i=0 ; i<SmoothIterations.value ; i++ )
-		{
-#pragma omp parallel for
-			for( int x=0 ; x<Resolution.values[0] ; x++ ) for( int y=0 ; y<Resolution.values[1] ; y++ ) for( int z=0 ; z<Resolution.values[2] ; z++ )
-			{
-				float weightSum = 0.f;
-				_voxelValues[ INDEX(x,y,z) ] = 0.f;
-				for( int xx=-1 ; xx<=1 ; xx++ ) for( int yy=-1 ; yy<=1 ; yy++ ) for( int zz=-1; zz<=1 ; zz++ )
-					if( x+xx>=0 && x+xx<Resolution.values[0] && y+yy>=0 && y+yy<Resolution.values[1] && z+zz>=0 && z+zz<Resolution.values[2] )
-					{
-						_voxelValues[ INDEX(x,y,z) ] += voxelValues[ INDEX(x+xx,y+yy,z+zz) ] * stencil[xx+1] * stencil[yy+1] * stencil[zz+1];
-						weightSum += stencil[xx+1] * stencil[yy+1] * stencil[zz+1];
-					}
-				_voxelValues[ INDEX(x,y,z) ] /= weightSum;
-			}
-			memcpy( voxelValues , _voxelValues , sizeof(float) * Resolution.values[0] * Resolution.values[1] * Resolution.values[2] );
-		}
-		DeletePointer( _voxelValues );
-	}
 #undef INDEX
 
 	std::vector< IsoVertex > vertices;
 	std::vector< std::vector< int > > polygons;
-	double t = Time();
-	ExtractIsoSurface( Resolution.values[0] , Resolution.values[1] , Resolution.values[2] , voxelValues , IsoValue.value , vertices , polygons , FullCaseTable.set , QuadraticFit.set , FlipOrientation.set );
-	printf( "Got iso-surface: %.2f(s)\n" , Time()-t );
+	ExtractIsoSurface( res , res, res, voxelValues , IsoValue, vertices , polygons , FullCaseTable, QuadraticFit, FlipOrientation);
+	printf( "Got iso-surface\n");
+
+	//TODO: When this pointer is passed in DON'T DELETE IT!!!!!!
 	DeletePointer( voxelValues );
 
-	if( Out.set )
-	{
-		std::vector< PlyVertex< float > > _vertices( vertices.size() );
-		for( int i=0 ; i<vertices.size() ; i++ ) for( int d=0 ; d<3 ; d++ ) _vertices[i].point[d] = vertices[i].p[d] * Dimensions.values[d];
-		if( Polygons.set )
-		{
-			PlyWritePolygons( Out.value , _vertices , polygons , PlyVertex< float >::WriteProperties , PlyVertex< float >::WriteComponents , PLY_BINARY_NATIVE );
-			printf( "Vertices / Polygons: %d / %d\n" , (int)vertices.size() , (int)polygons.size() );
-		}
-		else
-		{
-			std::vector< TriangleIndex > triangles;
-			MinimalAreaTriangulation< float > mat;
+	
+	//Replace this with obj file format.
+	std::vector< PlyVertex< float > > _vertices( vertices.size() );
+	for( int i=0 ; i<vertices.size() ; i++ ) for( int d=0 ; d<3 ; d++ ) _vertices[i].point[d] = vertices[i].p[d];// * Dimensions.values[d];
 
-			for( int i=0 ; i<polygons.size() ; i++ )
-			{
-				// To ensure that we have no more than two triangles adjacent on an edge,
-				// we avoid creating a minimial area triangulation when it could introduce a new
-				// edge that is on a face of a cube
-				bool isCofacial = false;
-				if( !NonManifold.set )
-					for( int j=0 ; j<(int)polygons[i].size() ; j++ ) for( int k=0 ; k<j ; k++ )
-						if( (j+1)%polygons[i].size()!=k && (k+1)%polygons[i].size()!=j )
-							if( IsoVertex::CoFacial( vertices[ polygons[i][j] ] , vertices[ polygons[i][k] ] ) ) isCofacial = true;
-				if( isCofacial )
-				{
-					TriangleIndex triangle;
-					PlyVertex< float > plyVertex;
-					for( int j=0 ; j<(int)polygons[i].size() ; j++ ) plyVertex.point += vertices[ polygons[i][j] ].p;
-					plyVertex.point /= (float)polygons[i].size();
-					int cIdx = (int)_vertices.size();
-					for( int d=0 ; d<3 ; d++ ) plyVertex.point[d] *= Dimensions.values[d];
-					_vertices.push_back( plyVertex );
-					for( int j=0 ; j<(int)polygons[i].size() ; j++ )
-					{
-						triangle[0] = polygons[i][j];
-						triangle[1] = polygons[i][(j+1)%polygons[i].size()];
-						triangle[2] = cIdx;
-						triangles.push_back( triangle );
-					}
-				}
-				else
-				{
-					std::vector< Point3D< float > > _polygon( polygons[i].size() );
-					std::vector< TriangleIndex > _triangles;
-					for( int j=0 ; j<polygons[i].size() ; j++ ) _polygon[j] = vertices[ polygons[i][j] ].p;
-					mat.GetTriangulation( _polygon , _triangles );
-					for( int j=0 ; j<_triangles.size() ; j++ )
-					{
-						TriangleIndex tri;
-						for( int k=0 ; k<3 ; k++ ) tri[k] = polygons[i][ _triangles[j][k] ];
-						triangles.push_back( tri );
-					}
-				}
-			}
-
-			PlyWriteTriangles( Out.value , _vertices , triangles , PlyVertex< float >::WriteProperties , PlyVertex< float >::WriteComponents , PLY_BINARY_NATIVE );
-			printf( "Vertices / Triangles: %d / %d\n" , (int)_vertices.size() , (int)triangles.size() );
-		}
-	}
+	PlyWritePolygons( out_path , _vertices , polygons , PlyVertex< float >::WriteProperties , PlyVertex< float >::WriteComponents , PLY_BINARY_NATIVE );
+	printf( "Vertices / Polygons: %d / %d\n" , (int)vertices.size() , (int)polygons.size() );
 
 	return EXIT_SUCCESS;
 }
